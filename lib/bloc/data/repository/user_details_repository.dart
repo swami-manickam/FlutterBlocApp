49e2ede746eb7eb1351edc2c6cef0fc859f541c9
SwamiManickavasagam <swami.manickavasagam@disys.com>
2024-02-13 18:47:56 +0530

    Initial Commit

---
+++ lib/bloc/data/repository/user_details_repository.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_sample_bloc_pattern/bloc/data/model/user_details.dart';
+import 'package:flutter_sample_bloc_pattern/bloc/data/provider/user_detais_provider.dart';
+
+class UserDetailsRepository {
+  final UserDetailsProvider _userDetailsProvider = UserDetailsProvider();
+
+  // Fetch the api response and pass it to bloc component
+  Future<UserDetails> fetchUserDetails() async =>
+      _userDetailsProvider.fetchUserDetails();
+}
