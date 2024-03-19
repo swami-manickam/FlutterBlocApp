a62d218fd0b300267fec0bb1f95768959c94e918
SwamiManickavasagam <swami.manickavasagam@disys.com>
2024-03-19 19:30:08 +0530

    Bottom navigation integration with bloc provider and API call

---
+++ lib/bloc/domain/navigationtab/landing_page_bloc.dart
@@ -0,0 +1,18 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/foundation.dart';
+
+part 'landing_page_event.dart';
+part 'landing_page_state.dart';
+
+class LandingPageBloc extends Bloc<LandingPageEvent, LandingPageState> {
+  LandingPageBloc() : super(const LandingPageInitial(tabIndex: 0)) {
+    on<LandingPageEvent>((event, emit) {
+      if (event is TabChange) {
+        if (kDebugMode) {
+          print(event.tabIndex);
+        }
+        emit(LandingPageInitial(tabIndex: event.tabIndex));
+      }
+    });
+  }
+}
