c62eb7e88c24e80b16f2ee0a381768e4f14aae90
SwamiManickavasagam <swami.manickavasagam@disys.com>
2024-04-19 15:02:50 +0530

    Profile card and Favourite list design added

---
+++ lib/presentation/widgets/product_cart_widgets.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProductCartWidget extends StatefulWidget {
+  const ProductCartWidget({super.key});
+
+  @override
+  State<ProductCartWidget> createState() => _ProductCartWidgetState();
+}
+
+class _ProductCartWidgetState extends State<ProductCartWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
